b4e2207d00fa60c41464b6bdb4a1e4760168a068
Luis Giuliani <154923735+jrgiuliani@users.noreply.github.com>
2024-04-12 02:38:18 +0000

    Entrega

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	// Logica
+	const [color, setColor]=useState("");
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="contenedor">
+			<div className="tubo"></div>
+			<div className="semaforo">
+				<div className={"boton red" + ((color == "red") ? " brillo": "")} onClick={()=>{
+					setColor("red");
+				}}></div>
+				<div className= {"boton yellow" + ((color == "yellow") ? " brillo": "")} onClick={()=>{
+					setColor("yellow");
+				}} ></div>
+				<div className= {"boton green" + ((color == "green") ? " brillo": "")} onClick={()=>{
+					setColor("green");
+				}}></div>
+			</div>
 		</div>
 	);
 };
